f8290b1e87b261bc523ab2edb4f35fb79d0b0d94
Andrey Lebedev <andreyelplus@gmail.com>
2025-07-23 10:31:20 +0300

    auto-increment placeholder for the new worlds

--- res/layouts/pages/new_world.xml.lua
+++ res/layouts/pages/new_world.xml.lua
@@ -24,6 +24,16 @@ function create_world()
     core.new_world(name, seed, generator)
 end
 
+function world_name_placeholder()
+    local name = "New World"
+    local number = 0
+    while world.exists(name) do
+        number = number + 1
+        name = "New World " .. tostring(number)
+    end
+    return name
+end
+
 function on_open()
     document.content_btn.text = string.format(
         "%s [%s]", gui.str("Content", "menu"), #pack.get_installed()
@@ -33,11 +43,12 @@ function on_open()
         settings.generator = generation.get_default_generator()
     end
     document.generator_btn.text = string.format(
-        "%s: %s", 
-        gui.str("World generator", "world"), 
+        "%s: %s",
+        gui.str("World generator", "world"),
         settings.generator_name(generation.get_generators()[settings.generator])
     )
     document.name_box.text = settings.name or ''
+    document.name_box.placeholder = world_name_placeholder()
     document.seed_box.text = settings.seed or ''
     document.seed_box.placeholder = tostring(math.random()):sub(3)
 end
