422d143e7f8a6e4633bef014bd1effb0099721cb
MihailRis <mexaruc@gmail.com>
2025-09-10 00:58:32 +0300

    fix: generated item does not inherit block tags

--- src/content/ContentLoader.cpp
+++ src/content/ContentLoader.cpp
@@ -288,6 +288,7 @@ void ContentLoader::loadContent(const dv::value& root) {
             item.iconType = ItemIconType::BLOCK;
             item.icon = def.name;
             item.placingBlock = def.name;
+            item.tags = def.tags;
     
             for (uint j = 0; j < 4; j++) {
                 item.emission[j] = def.emission[j];
