2204cb795e80d48ee75b86c84700a5c66ef4a75f
MihailRis <mexaruc@gmail.com>
2025-09-14 16:11:12 +0300

    fix network_http test

--- dev/tests/network_http.lua
+++ dev/tests/network_http.lua
@@ -3,6 +3,9 @@ local response_received = false
 network.get("https://api.github.com/repos/MihailRis/VoxelEngine-Cpp/releases/latest", function (s)
     print(json.parse(s).name)
     response_received = true
+end, function (code)
+    print("repond with code", code)
+    response_received = true
 end)
 
 app.sleep_until(function () return response_received end, nil, 10)
