dc21a8ea5156de3db88f71d1a240e4285242b3ee
MihailRis <mexaruc@gmail.com>
2025-09-16 19:12:25 +0300

    update random.hpp

--- src/util/random.hpp
+++ src/util/random.hpp
@@ -5,10 +5,10 @@
 #include <algorithm>
 
 namespace util {
-    template <class T = std::mt19937, std::size_t N = T::state_size * sizeof(typename T::result_type)>
-    typename std::enable_if<N, T>::type seeded_random_engine(
-        std::random_device& source
-    ) {
+    template <
+        class T = std::mt19937,
+        std::size_t N = T::state_size * sizeof(typename T::result_type)>
+    auto seeded_random_engine(std::random_device& source) {
         std::random_device::result_type randomData[(N - 1) / sizeof(source()) + 1];
         std::generate(std::begin(randomData), std::end(randomData), std::ref(source));
         std::seed_seq seeds(std::begin(randomData), std::end(randomData));
