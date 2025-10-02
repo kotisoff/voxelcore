5460c716cc9c85f3ce1a07d30b2d54bc2f2f69a0
MihailRis <mexaruc@gmail.com>
2025-10-02 23:50:14 +0300

    fix entities visibility

--- src/objects/rigging.cpp
+++ src/objects/rigging.cpp
@@ -141,7 +141,7 @@ void SkeletonConfig::update(
             build_matrix(rotation, interpolation.getCurrent())
         );
     } else {
-        update(0, skeleton, root.get(), rotation);
+        update(0, skeleton, root.get(), build_matrix(rotation, position));
     }
 }
 
