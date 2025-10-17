6c01d57d53d7f7e4ade881f10c55826fefe11a65
MihailRis <mexaruc@gmail.com>
2025-10-17 17:35:22 +0300

    cleanup

--- src/graphics/core/Shadows.cpp
+++ src/graphics/core/Shadows.cpp
@@ -1,13 +1,11 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/norm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "Shadows.hpp"
 
 #include <GL/glew.h>
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/norm.hpp>
-#include <glm/gtc/constants.hpp>
-#include "glm/gtc/matrix_transform.hpp"
 
 #include "assets/Assets.hpp"
 #include "graphics/core/DrawContext.hpp"
