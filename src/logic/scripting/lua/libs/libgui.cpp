5ae4c1b27b6293d346ef253939bbe2c6cf80af3b
MihailRis <mexaruc@gmail.com>
2026-09-17 21:58:38 +0300

    add gui.screenshot

--- src/logic/scripting/lua/libs/libgui.cpp
+++ src/logic/scripting/lua/libs/libgui.cpp
@@ -25,6 +25,7 @@
 #include "items/Inventories.hpp"
 #include "util/stringutil.hpp"
 #include "world/Level.hpp"
+#include "window/Window.hpp"
 #include "../usertypes/lua_type_canvas.hpp"
 
 using namespace gui;
@@ -1218,7 +1219,16 @@ static int l_get_active_frame(lua::State* L) {
     return lua::pushstring(L, frame->getId());
 }
 
+static int l_screenshot(lua::State* L) {
+    if (engine->isHeadless()) {
+        return 0;
+    }
+    auto image = engine->getWindow().takeScreenshot();
+    return lua::newuserdata<lua::LuaCanvas>(L, nullptr, std::move(image));
+}
+
 const luaL_Reg guilib[] = {
+    {"screenshot", lua::wrap<l_screenshot>},
     {"get_viewport", lua::wrap<l_gui_getviewport>},
     {"getattr", lua::wrap<l_gui_getattr>},
     {"setattr", lua::wrap<l_gui_setattr>},
